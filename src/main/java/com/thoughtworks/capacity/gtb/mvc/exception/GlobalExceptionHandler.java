ccc04bf57afd4fcb20f23b9e24e0bd88b9877853
EscapedSheep <sfgmdf001@126.com>
2020-09-10 13:51:33 +0800

    [maida] fix: fix throw error of login api

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ConstraintViolationException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -19,6 +21,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({UserNameAlreadyExistedException.class, UserNameOrPasswordNotValidException.class})
     public ResponseEntity<ErrorResponse> handleUserNameExistedException(Exception exception) {
         ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException exception) {
+        String message = exception.getMessage();
+        ErrorResponse errorResponse = new ErrorResponse(message);
         return ResponseEntity.badRequest().body(errorResponse);
     }
 }
